c66b592a2e207d1c93772e424cbc31318ec5af10
Dmitry <cryinstone@gmail.com>
2026-07-03 14:11:19 +1000

    init

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MaestroPlayground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
